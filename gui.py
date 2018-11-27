59dff3bfbebf261fe02752ea63311e777ed2a356
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2018-11-27 23:08:46 +0300

    improved GUI

--- gui.py
+++ gui.py
@@ -1,16 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
+from check_input import is_key_valid
+from tkinter import filedialog
+from encryption import convert_str_to_int, get_file_bits, handle_entry_bin
+import binaryEncipher
+from time import time
 
-# constant colors and fonts using in this GUI
+# Constant colors and fonts using in this GUI
 HEADER_FONT = ('Open Sans', 15)
 BUTTON_FONT = ('Open Sans', 12)
 LABEL_FONT = ('Open Sans light', 10)
+LABEL_SUCCESS_FONT = ('Open Sans', 10)
 ENTRY_FONT = ('Open Sans', 10)
+STATUS_FONT = ('Open Sans Bold', 12)
 DARK_GRAY_COlOR = '#393E41'
 GRAY_COLOR = '#D3D0CB'
 LIGHT_GRAY_COLOR = '#E7E5DF'
 YELLOW_COLOR = '#E7BB41'
 BLUE_COLOR = '#44BBA4'
+
+# Other settings
+DEBUG = True
+SIZE_OF_BITS = 1024
+INPUT_FILE_BITS = True
 
 
 class CryptApp(tk.Tk):
@@ -49,6 +61,10 @@ class MainPage(ttk.Frame):
         and save it to another file.
     """
     def __init__(self, parent, controller):
+
+        self.input_file_name = ''
+        self.output_file_name = ''
+
         ttk.Frame.__init__(self, parent)
         style = ttk.Style()
         style.configure('TFrame', background=DARK_GRAY_COlOR)
@@ -56,64 +72,296 @@ class MainPage(ttk.Frame):
         self.columnconfigure(1, weight=3)
         self.columnconfigure(2, weight=1)
         self.columnconfigure(3, weight=3)
-        self.rowconfigure(6, weight=2)
+        self.rowconfigure(7, weight=2)
 
-        style.configure('TButton', font=BUTTON_FONT, padding=10, relief='flat',
+        # Initializing styles
+        style.configure('TButton',
+                        font=BUTTON_FONT,
+                        padding=10,
+                        relief='flat',
                         foreground=DARK_GRAY_COlOR)
-        style.configure('open.TButton', background=YELLOW_COLOR)
-        style.configure('encrypt.TButton', background=YELLOW_COLOR)
-        style.configure('key.TEntry', font=ENTRY_FONT, background=GRAY_COLOR, foreground=DARK_GRAY_COlOR,
+        style.configure('open.TButton',
+                        background=YELLOW_COLOR)
+        style.configure('encrypt.TButton',
+                        background=YELLOW_COLOR)
+        style.configure('key.TEntry',
+                        font=ENTRY_FONT,
+                        background=GRAY_COLOR,
+                        foreground=DARK_GRAY_COlOR,
                         padding='10 2 2 2')
-        style.configure('TLabel', font=LABEL_FONT, background=DARK_GRAY_COlOR, foreground=LIGHT_GRAY_COLOR)
-        style.configure("Vertical.TScrollbar", gripcount=0,
-                        background=GRAY_COLOR, darkcolor=DARK_GRAY_COlOR, lightcolor=LIGHT_GRAY_COLOR,
-                        troughcolor=LIGHT_GRAY_COLOR, bordercolor=GRAY_COLOR, arrowcolor=DARK_GRAY_COlOR,
+        style.configure('TLabel',
+                        font=LABEL_FONT,
+                        background=DARK_GRAY_COlOR,
+                        foreground=LIGHT_GRAY_COLOR)
+        style.configure('success.TLabel',
+                        font=LABEL_SUCCESS_FONT,
+                        background=DARK_GRAY_COlOR,
+                        foreground=BLUE_COLOR)
+        style.configure("Vertical.TScrollbar",
+                        gripcount=0,
+                        background=DARK_GRAY_COlOR,
+                        darkcolor=DARK_GRAY_COlOR,
+                        lightcolor=GRAY_COLOR,
+                        troughcolor=LIGHT_GRAY_COLOR,
+                        bordercolor=GRAY_COLOR,
+                        arrowcolor=GRAY_COLOR,
                         relief='flat')
 
-        header_label = tk.Label(self, text="Stream Encryption", font=HEADER_FONT,
-                                background=DARK_GRAY_COlOR, foreground=BLUE_COLOR, justify='center')
+        header_label = tk.Label(self, text="Stream Encryption",
+                                font=HEADER_FONT,
+                                background=DARK_GRAY_COlOR,
+                                foreground=BLUE_COLOR,
+                                justify='center')
         header_label.grid(padx=10, pady=10, column=0, row=0, columnspan=4, sticky='n e s w')
 
-        open_file_button = ttk.Button(self, text='Open file...', style='open.TButton')
+        # File open button
+        open_file_button = ttk.Button(self, text='Open file...', style='open.TButton', command=self.open_file)
         open_file_button.grid(padx=10, pady=0, column=0, row=2, sticky='n w e s ')
 
-        open_file_status_label = ttk.Label(self, text='Status: File is not opened!')
-        open_file_status_label.grid(padx=10, column=0, row=3, sticky='n w')
+        self.open_file_status_label = ttk.Label(self, text='File is not opened!')
+        self.open_file_status_label.grid(padx=10, column=0, row=3, sticky='n w')
 
-        encrypt_decrypt_key_label = ttk.Label(self, text='Encryption/ Decryption key:')
-        encrypt_decrypt_key_label.grid(padx=10, column=2, row=1, sticky='n w')
+        # Encryption key input
+        self.encrypt_decrypt_key_label = ttk.Label(self, text='Encryption/ Decryption key:')
+        self.encrypt_decrypt_key_label.grid(padx=10, column=2, row=1, sticky='n w')
 
-        self.encrypt_decrypt_key_entry = ttk.Entry(self, style='key.TEntry')
+        vcmd = (self.register(self._change_key_len),
+                '%P', '%V')
+        self.encryption_key = tk.StringVar()
+        self.encrypt_decrypt_key_entry = ttk.Entry(self,
+                                                   style='key.TEntry',
+                                                   validate='all',
+                                                   validatecommand=vcmd,
+                                                   textvariable=self.encryption_key)
         self.encrypt_decrypt_key_entry.grid(padx=10, column=2, row=2, sticky='n w e s')
 
-        encrypt_decrypt_key_label_len = ttk.Label(self, text='0/23')
-        encrypt_decrypt_key_label_len.grid(padx=10, column=2, row=3, sticky='n w')
+        self.encrypt_decrypt_key_label_len = tk.Label(self,
+                                                      text='0/23',
+                                                      background=DARK_GRAY_COlOR,
+                                                      foreground=LIGHT_GRAY_COLOR)
+        self.encrypt_decrypt_key_label_len.grid(padx=10, column=2, row=3, sticky='n w')
 
-        encrypt_button = ttk.Button(self, text='Encrypt', style='encrypt.TButton')
+        # Encrypt button
+        encrypt_button = ttk.Button(self,
+                                    text='Encrypt',
+                                    style='encrypt.TButton',
+                                    command=lambda: self.encrypt_file(True))
         encrypt_button.grid(padx=10, pady=25, column=0, columnspan=2, row=4, sticky='n w e s')
 
-        decrypt_button = ttk.Button(self, text='Decrypt', style='encrypt.TButton')
+        # Decrypt button
+        decrypt_button = ttk.Button(self,
+                                    text='Decrypt',
+                                    style='encrypt.TButton',
+                                    command=lambda: self.encrypt_file(False))
         decrypt_button.grid(padx=10, pady=25, column=2, columnspan=2, row=4, sticky='n w e s')
 
-        input_file_bytes_label = ttk.Label(self, text='Input file bytes:')
-        input_file_bytes_label.grid(padx=10, column=0, row=5, sticky='n w')
+        self.encrypt_decrypt_status_label = tk.Label(self,
+                                                     text='',
+                                                     background=DARK_GRAY_COlOR,
+                                                     foreground=LIGHT_GRAY_COLOR,
+                                                     font=STATUS_FONT)
+        self.encrypt_decrypt_status_label.grid(padx=10, column=0, columnspan=4, row=5, sticky='n w e')
 
-        output_file_bytes_label = ttk.Label(self, text='Output file bytes:')
-        output_file_bytes_label.grid(padx=10, column=2, row=5, sticky='n w')
+        if not INPUT_FILE_BITS:
+            # Input & Output file bytes fields
+            encrypt_file_bytes_label = ttk.Label(self, text='Encryption key bytes:')
+            encrypt_file_bytes_label.grid(padx=10, pady=5, column=0, row=6, sticky='n w')
 
-        input_file_bytes = tk.Text(self, font=ENTRY_FONT, background=LIGHT_GRAY_COLOR,
-                                   foreground=DARK_GRAY_COlOR, relief='flat')
-        input_file_bytes.grid(padx=10, pady=10, column=0, columnspan=2, row=6, sticky='n w e s')
+            output_file_bytes_label = ttk.Label(self, text='Output file bytes:')
+            output_file_bytes_label.grid(padx=10, pady=5, column=2, row=6, sticky='n w')
 
-        input_scroll = ttk.Scrollbar(input_file_bytes, orient='vertical')
-        input_scroll.pack(side='right', fill='y')
+            key_file_frame = tk.Frame(self)
 
-        output_file_bytes = tk.Text(self, font=ENTRY_FONT, background=LIGHT_GRAY_COLOR,
-                                    foreground=DARK_GRAY_COlOR, relief='flat')
-        output_file_bytes.grid(padx=10, pady=10, column=2, columnspan=2, row=6, sticky='n w e s')
+            key_scroll = ttk.Scrollbar(key_file_frame, orient='vertical')
+            key_scroll.pack(side='right', fill='y')
+            self.key_file_bytes = tk.Text(key_file_frame,
+                                          font=ENTRY_FONT,
+                                          background=LIGHT_GRAY_COLOR,
+                                          foreground=DARK_GRAY_COlOR,
+                                          relief='flat')
+            self.key_file_bytes.pack(fill='both')
+            key_file_frame.grid(padx=10, pady=10, column=0, columnspan=2, row=7, sticky='n w e s')
 
-        output_scroll = ttk.Scrollbar(output_file_bytes)
-        output_scroll.pack(side='right', fill='y')
+            key_scroll.config(command=self.key_file_bytes.yview)
+            self.key_file_bytes.config(yscrollcommand=key_scroll.set, state=tk.DISABLED)
+
+            output_file_frame = tk.Frame(self)
+            output_scroll = ttk.Scrollbar(output_file_frame, orient='vertical')
+            output_scroll.pack(side='right', fill='y')
+            self.output_file_bytes = tk.Text(output_file_frame,
+                                             font=ENTRY_FONT,
+                                             background=LIGHT_GRAY_COLOR,
+                                             foreground=DARK_GRAY_COlOR,
+                                             relief='flat')
+            self.output_file_bytes.pack(fill='both')
+            output_file_frame.grid(padx=10, pady=10, column=2, columnspan=2, row=7, sticky='n w e s')
+            output_scroll.config(command=self.output_file_bytes.yview)
+            self.output_file_bytes.config(yscrollcommand=output_scroll.set, state=tk.DISABLED)
+        else:
+            input_output_key_frame = tk.Frame(self, background=DARK_GRAY_COlOR)
+            input_output_key_frame.columnconfigure(0, weight=1)
+            input_output_key_frame.columnconfigure(1, weight=1)
+            input_output_key_frame.columnconfigure(2, weight=1)
+            # Input & Output file bytes fields
+            input_file_bytes_label = ttk.Label(input_output_key_frame, text='Input file bytes:')
+            input_file_bytes_label.grid(padx=10, pady=5, column=0, row=0, sticky='n w')
+            key_file_bytes_label = ttk.Label(input_output_key_frame, text='Encryption key bytes:')
+            key_file_bytes_label.grid(padx=10, pady=5, column=1, row=0, sticky='n w')
+            output_file_bytes_label = ttk.Label(input_output_key_frame, text='Output file bytes:')
+            output_file_bytes_label.grid(padx=10, pady=5, column=2, row=0, sticky='n w')
+
+            input_file_frame = tk.Frame(input_output_key_frame)
+            input_scroll = ttk.Scrollbar(input_file_frame, orient='vertical')
+            input_scroll.pack(side='right', fill='y')
+            self.input_file_bytes = tk.Text(input_file_frame,
+                                            font=ENTRY_FONT,
+                                            background=LIGHT_GRAY_COLOR,
+                                            foreground=DARK_GRAY_COlOR,
+                                            relief='flat')
+            self.input_file_bytes.pack(fill='both')
+            input_file_frame.grid(padx=10, pady=10, column=0, row=1, sticky='n w e s')
+
+            input_scroll.config(command=self.input_file_bytes.yview)
+            self.input_file_bytes.config(yscrollcommand=input_scroll.set, state=tk.DISABLED)
+
+            key_file_frame = tk.Frame(input_output_key_frame)
+            key_scroll = ttk.Scrollbar(key_file_frame, orient='vertical')
+            key_scroll.pack(side='right', fill='y')
+            self.key_file_bytes = tk.Text(key_file_frame,
+                                          font=ENTRY_FONT,
+                                          background=LIGHT_GRAY_COLOR,
+                                          foreground=DARK_GRAY_COlOR,
+                                          relief='flat')
+            self.key_file_bytes.pack(fill='both')
+            key_file_frame.grid(padx=10, pady=10, column=1, row=1, sticky='n w e s')
+
+            key_scroll.config(command=self.key_file_bytes.yview)
+            self.key_file_bytes.config(yscrollcommand=key_scroll.set, state=tk.DISABLED)
+
+            output_file_frame = tk.Frame(input_output_key_frame)
+            output_scroll = ttk.Scrollbar(output_file_frame, orient='vertical')
+            output_scroll.pack(side='right', fill='y')
+            self.output_file_bytes = tk.Text(output_file_frame,
+                                             font=ENTRY_FONT,
+                                             background=LIGHT_GRAY_COLOR,
+                                             foreground=DARK_GRAY_COlOR,
+                                             relief='flat')
+            self.output_file_bytes.pack(fill='both')
+            output_file_frame.grid(padx=10, pady=10, column=2, row=1, sticky='n w e s')
+
+            output_scroll.config(command=self.output_file_bytes.yview)
+            self.output_file_bytes.config(yscrollcommand=output_scroll.set, state=tk.DISABLED)
+
+            input_output_key_frame.grid(padx=10, pady=10, column=0, columnspan=4, row=6, rowspan=2, sticky='n w e s')
+
+    def _change_key_len(self, text, callback):
+        """
+        Checking input Encryption key
+        :param text: encryption key
+        :return: True if key is valid, else False
+        """
+        if callback == 'focusout':
+            text = handle_entry_bin(text)
+            self.encryption_key.set(text)
+        if is_key_valid(text) and len(text) < 23:
+            self.encrypt_decrypt_key_label_len.configure(text='{}/23'.format(len(text)), foreground=LIGHT_GRAY_COLOR)
+        elif is_key_valid(text) and len(text) == 23:
+            self.encrypt_decrypt_key_label_len.configure(text='23/23', foreground=BLUE_COLOR)
+        else:
+            self.encrypt_decrypt_key_label_len.configure(text='Value is invalid, please, fix it!',
+                                                         foreground=YELLOW_COLOR)
+        return True
+
+    def open_file(self):
+        """
+        Handling open file dialog
+        :return: None
+        """
+        self.input_file_name = filedialog.askopenfilename(title="Select file",
+                                                          filetypes=(("all files", "*.*"),))
+        if self.input_file_name:
+            short_file_name = self.input_file_name.split('/')[-1]
+            self.open_file_status_label.configure(text='File opened: {}'.format(short_file_name),
+                                                  style='success.TLabel')
+            self.encrypt_decrypt_status_label.config(text='')
+            debug_message('File {} opened successful!'.format(short_file_name))
+
+    def save_file(self):
+        """
+        Handling saving file dialog
+        :return: None
+        """
+        self.output_file_name = filedialog.asksaveasfilename(title="Save file as...",
+                                                             filetypes=(("all files", "*.*"),))
+        if self.output_file_name:
+            return True
+        else:
+            return False
+
+    def encrypt_file(self, is_encrypt):
+        """
+        Encrypting file if it's opened
+        :argument is_encrypt: True if Encrypt, else False
+        :return: none
+        """
+        if is_encrypt:
+            MSG_ENCRYPT = 'Encryption'
+        else:
+            MSG_ENCRYPT = 'Decryption'
+        if not self.input_file_name:
+            self.encrypt_decrypt_status_label.configure(text='{} failed! (File is not opened)'.format(MSG_ENCRYPT),
+                                                        foreground=YELLOW_COLOR)
+            debug_message('File is not opened!')
+        elif not len(handle_entry_bin(self.encryption_key.get())) == 23:
+            self.encrypt_decrypt_status_label.configure(text='Encryption key is incorrect!',
+                                                        foreground=YELLOW_COLOR)
+        else:
+            if self.save_file():
+                key = convert_str_to_int(self.encryption_key.get())
+                operation_time = time()
+                if binaryEncipher.encryptFile(self.input_file_name, self.output_file_name, key):
+                    operation_time = time() - operation_time
+                    self.encrypt_decrypt_status_label.configure(
+                        text='%s successful! Total time: %.2f sec.' % (MSG_ENCRYPT, operation_time),
+                        foreground=BLUE_COLOR)
+
+                    self.key_file_bytes.config(state=tk.NORMAL)
+                    self.output_file_bytes.config(state=tk.NORMAL)
+
+                    self.key_file_bytes.delete(1.0, tk.END)
+                    self.output_file_bytes.delete(1.0, tk.END)
+
+                    self.key_file_bytes.insert(tk.END, get_file_bits('tmp.bin', SIZE_OF_BITS))
+                    self.output_file_bytes.insert(tk.END, get_file_bits(self.output_file_name, SIZE_OF_BITS))
+
+                    self.key_file_bytes.config(state=tk.DISABLED)
+                    self.output_file_bytes.config(state=tk.DISABLED)
+                    if INPUT_FILE_BITS:
+                        self.input_file_bytes.config(state=tk.NORMAL)
+                        self.input_file_bytes.delete(1.0, tk.END)
+                        self.input_file_bytes.insert(tk.END, get_file_bits(self.input_file_name, SIZE_OF_BITS))
+                        self.input_file_bytes.config(state=tk.DISABLED)
+                else:
+                    self.encrypt_decrypt_status_label.configure(text='{} failed!'.format(MSG_ENCRYPT),
+                                                                foreground=YELLOW_COLOR)
+                debug_message('{} successful!'.format(MSG_ENCRYPT))
+                debug_message('Saved as {}'.format(self.output_file_name.split('/')[-1]))
+            else:
+                self.encrypt_decrypt_status_label.config(
+                    text='{} failed! (File to save is not selected)'.format(MSG_ENCRYPT),
+                    foreground=YELLOW_COLOR)
+                debug_message('File to save is not selected!')
+
+
+def debug_message(message):
+    """
+    Sending debug message if DEBUG is enabled
+    :param message: message to send
+    :return:
+    """
+    if DEBUG:
+        print('[DEBUG]: {}'.format(message))
+    return 0
 
 
 app = CryptApp()
